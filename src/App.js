be2f48fff1cbe0565bd0b8b0b2b27dc35e80be37
hmhuan <minhhuanhuynh289@gmail.com>
2022-05-09 15:00:16 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [films, setFilms] = useState([]);
+
+  useEffect(() => {
+    getFilms();
+  }, []);
+
+  async function getFilms() {
+    const response = await fetch("https://ghibliapi.herokuapp.com/films");
+    const data = await response.json();
+    console.log(data);
+    setFilms(data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src='logo.jpg' alt="logo"></img>
+      <div className="container">
+        {films.map((film) => (
+          <div key={film.id} className="card">
+            <h1>{film.title}</h1>
+            <p>{film.description.substring(0, 289) + '...'}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
